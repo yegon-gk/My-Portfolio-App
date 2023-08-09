b72e08b91f5a2371a96694f624efc5fce6b98c54
KIPROTYCH <kiprotych@gmail.com>
2023-08-09 11:12:57 +0300

    Modified these files

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {AiOutlineMenu} from "react-icons/ai";
+import {AiOutlineHome} from "react-icons/ai";
 
 
 const Sidenav = () => {
@@ -10,7 +11,22 @@ const Sidenav = () => {
     
     return (
         <div>
-            <AiOutlineMenu className="absolute top-4 right-4 z-[99] md:hidden" />
+            <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
+            {
+                nav ? (
+
+                    <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+                        <a href="#main">
+                            <AiOutlineHome size={20} />
+                            <span className="pl-4"> Home</span>
+                        </a>
+                    </div>
+                )
+                :(
+                    <div></div>
+
+                )
+            }
         </div>
     );
 };
